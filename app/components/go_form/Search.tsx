cbf32dc61d1ce90451d5d47fc3dcf2accff29b24
dvegaa20 <diego.vega.camacho@gmail.com>
2025-01-31 13:57:44 -0600

    feat(component): add Search component for form searching functionality

---
+++ app/components/go_form/Search.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchInput from "./search/SearchInput";
+
+export default async function Search() {
+  // const forms = await fetchForms();
+  return (
+    <>
+      <SearchInput />
+      {/* <SearchMobile /> */}
+    </>
+  );
+}
