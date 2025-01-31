da177ff13c0df82c78468ce2019d6b0caed51902
dvegaa20 <diego.vega.camacho@gmail.com>
2025-01-31 13:58:25 -0600

    feat(component): add Footer component for site footer with navigation links

---
+++ app/components/landing/Footer.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import React from "react";
+import { BackgroundBeamsWithCollision } from "../ui/background-beams-with-collision";
+
+export default function Footer() {
+  return (
+    <BackgroundBeamsWithCollision>
+      <footer className="w-full py-6 border-t flex justify-center">
+        <div className="container max-w-5xl px-4 md:px-6 flex flex-col sm:flex-row items-center justify-between">
+          <p className="text-xs text-gray-500 dark:text-gray-400">
+            © 2024 GoForms. All rights reserved.
+          </p>
+          <nav className="flex gap-4 sm:gap-6 mt-4 sm:mt-0">
+            <Link
+              className="text-xs hover:underline underline-offset-4"
+              href="#"
+            >
+              Terms of Service
+            </Link>
+            <Link
+              className="text-xs hover:underline underline-offset-4"
+              href="#"
+            >
+              Privacy
+            </Link>
+          </nav>
+        </div>
+      </footer>
+    </BackgroundBeamsWithCollision>
+  );
+}
