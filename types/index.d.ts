0010da12117055aa294e80fe146fd48212678470
dvegaa20 <diego.vega.camacho@gmail.com>
2025-02-05 10:31:23 -0600

    feat(types): add Form and Question interfaces for form structure

---
+++ types/index.d.ts
@@ -0,0 +1,24 @@
+declare interface Form {
+  id: string;
+  title: string;
+  description: string;
+  topic: string;
+  questions: Question[];
+  createdAt: string;
+  createdBy: string;
+}
+
+declare interface Question {
+  order: number;
+  question_type:
+    | "integer"
+    | "multiple-choice"
+    | "text"
+    | "number"
+    | "boolean"
+    | "rating"
+    | "email";
+  question_title: string;
+  question_description?: string;
+  options?: string[];
+}
