645f7f10826cbbe47da07683e17244a3b13e5fd4
dvegaa20 <diego.vega.camacho@gmail.com>
2025-02-05 10:26:48 -0600

    feat(admin): add LinkTabContent component for displaying and copying form link

---
+++ app/components/go_form/form/LinkTabContent.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React from "react";
+import { useParams } from "next/navigation";
+import { TabsContent } from "@/components/ui/tabs";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { DialogClose } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+
+export default function LinkTabContent() {
+  const params = useParams();
+  const id = params.id;
+  const url = `${window.location.origin}/forms/${id}`;
+  return (
+    <TabsContent value="link" className="px-4 py-4 space-y-3">
+      <div className="grid w-full items-center gap-3">
+        <Label htmlFor="link" className="text-base">
+          Link
+        </Label>
+        <Input type="url" id="link" value={url} readOnly />
+      </div>
+      <div className="flex items-end justify-end space-x-2">
+        <DialogClose asChild>
+          <Button size="sm">Cancel</Button>
+        </DialogClose>
+        <Button
+          size="sm"
+          onClick={() => {
+            navigator.clipboard.writeText(url);
+            toast.success("Link copied to clipboard");
+          }}
+        >
+          Copy link
+        </Button>
+      </div>
+    </TabsContent>
+  );
+}
