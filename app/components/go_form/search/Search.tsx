29c69702e3b466c5610fa6609dc5c3c126697773
dvegaa20 <diego.vega.camacho@gmail.com>
2025-02-05 10:18:11 -0600

    feat(search): refactor Search component to include form fetching and mobile view

---
+++ app/components/go_form/search/Search.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SearchInput from "./SearchInput";
+import { fetchAllForms } from "@/../lib/actions/actions";
+import SearchMobile from "./SearchMobile";
+
+export default async function Search() {
+  const forms = (await fetchAllForms()) as Form[];
+
+  return (
+    <>
+      <SearchInput forms={forms} />
+      <SearchMobile />
+    </>
+  );
+}
