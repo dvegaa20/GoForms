8d116fd8c103b98479bbca11ab775204dcab0581
dvegaa20 <diego.vega.camacho@gmail.com>
2025-02-05 10:30:12 -0600

    feat(ui): update DashboardPage to fetch and post current user data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,19 @@
 import Header from "@/components/go_form/Header";
 import RecentForms from "@/components/go_form/RecentForms";
+import { currentUser } from "@clerk/nextjs/server";
+import { postUserData } from "../../lib/actions/actions";
 import React from "react";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const { id, firstName, lastName, emailAddresses } = await currentUser();
+
+  await postUserData({
+    id,
+    firstName,
+    lastName,
+    email: emailAddresses[0].emailAddress,
+  });
+
   return (
     <div>
       <Header />
