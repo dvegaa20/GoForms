cfeb95783805f3fc0375c4dd2de532beabb1fe56
dvegaa20 <diego.vega.camacho@gmail.com>
2025-02-05 10:28:08 -0600

    feat(search): enhance SearchInput component with keyboard shortcuts and dialog integration

--- app/components/go_form/search/SearchInput.tsx
+++ app/components/go_form/search/SearchInput.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import {
   Command,
   CommandDialog,
@@ -12,18 +12,67 @@ import {
   CommandSeparator,
   CommandShortcut,
 } from "@/components/ui/command";
-import { SearchIcon } from "lucide-react";
+import { SearchIcon, Command as LucideCommand, ChevronUp } from "lucide-react";
+import { useDialogStore } from "@/../store/store";
+import { useRouter } from "next/navigation";
+import { DialogTitle } from "@/components/ui/dialog";
 
-export default function SearchInput() {
-  //   const { close, isOpen, open } = useDialogStore();
+export default function SearchInput({ forms }: { forms: Form[] }) {
+  const router = useRouter();
+  const { close, isOpen, open } = useDialogStore();
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        open();
+      }
+    };
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, [open]);
+
+  const runCommand = useCallback(
+    (command: () => unknown) => {
+      close();
+      command();
+    },
+    [close]
+  );
+
   return (
-    <div>
-      <div className="hidden md:flex flex-1 items-center justify-between w-full lg:w-auto max-w-xl lg:min-w-[270px] py-3 px-4 gap-x-6 rounded-md cursor-pointer bg-gray-100 transition-colors">
+    <>
+      <div
+        onClick={open}
+        className="hidden md:flex flex-1 items-center justify-between w-full lg:w-[800px] max-w-xl py-3 px-4 gap-x-6 rounded-md cursor-pointer bg-gray-100 transition-colors"
+      >
         <div className="flex items-center gap-x-2">
-          <SearchIcon className="w-6 h-6 text-neutral-500" />
-          <p className="text-neutral-500 font-light">Search</p>
+          <SearchIcon className="w-4 h-4 text-neutral-500" />
+          <p className="text-neutral-500 font-light flex items-center text-sm">
+            Search or Press <LucideCommand className="w-4 h-4 mx-1" />k or
+            <ChevronUp className="w-4 h-4 mx-1" /> k
+          </p>
         </div>
       </div>
-    </div>
+      <CommandDialog open={isOpen} onOpenChange={close}>
+        <DialogTitle></DialogTitle>
+        <CommandInput placeholder="Search" />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup>
+            {forms.map((form) => (
+              <CommandItem
+                key={form.id}
+                onSelect={() =>
+                  runCommand(() => router.push(`/dashboard/${form.id}`))
+                }
+              >
+                {form.title}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
   );
 }
