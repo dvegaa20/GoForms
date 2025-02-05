98c6ec62f24201778205c89e8d63b0b84e6b1952
dvegaa20 <diego.vega.camacho@gmail.com>
2025-02-05 10:30:36 -0600

    feat(ui): create FormIdPage component for dynamic form rendering by ID

---
+++ app/dashboard/forms/[id]/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { fetchFormById, getQuestions } from "@/../lib/actions/actions";
+import { notFound } from "next/navigation";
+import MainForm from "@/components/go_form/form/MainForm";
+
+export default async function FormIdPage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const form = (await fetchFormById({ id })) as Form[];
+  const formQuestions = (await getQuestions({ id })) as Question[];
+
+  if (!form) {
+    notFound();
+  }
+
+  return <MainForm form={form[0]} formQuestions={formQuestions} />;
+}
