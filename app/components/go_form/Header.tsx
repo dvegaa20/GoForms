a264d1f1e6805ae1014dacd7b019e14589cfa50c
dvegaa20 <diego.vega.camacho@gmail.com>
2025-02-05 10:19:21 -0600

    feat(header): enhance Header component to include AdminPanel for admin users

--- app/components/go_form/Header.tsx
+++ app/components/go_form/Header.tsx
@@ -1,20 +1,29 @@
 import Link from "next/link";
 import React from "react";
 import { Layers } from "lucide-react";
+import Search from "./search/Search";
+import AdminPanel from "./admin/AdminPanel";
 import { UserButton } from "@clerk/nextjs";
-import Search from "./Search";
+import { currentUser } from "@clerk/nextjs/server";
+import { fetchUserStatus } from "@/../lib/actions/actions";
 
-export default function Header() {
+export default async function Header() {
+  const user = await currentUser();
+  const isAdmin = await fetchUserStatus({ id: user.id });
+
   return (
-    <header className="flex justify-between sticky top-0 gap-x-4 px-4 py-2 bg-white z-50 max-w-screen-2xl mx-auto">
+    <header className="flex justify-between items-center sticky top-0 gap-x-4 px-4 py-2 bg-white z-50 max-w-screen-2xl mx-auto shadow">
       <div className="flex items-center space-x-2">
         <Link href={"/"} className="flex items-center space-x-2">
           <Layers className="h-6 w-6 text-primary" />
           <span className="ml-2 text-xl font-bold">GoForms</span>
         </Link>
       </div>
-      <Search />
-      <UserButton />
+      <div className="flex items-center space-x-4">
+        <Search />
+        {isAdmin[0].admin && <AdminPanel />}
+        <UserButton />
+      </div>
     </header>
   );
 }
