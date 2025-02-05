87d8d4d46138450612a779da75953324be9edc3c
dvegaa20 <diego.vega.camacho@gmail.com>
2025-02-05 10:25:44 -0600

    feat(admin): add FormList component for displaying all forms

---
+++ app/components/go_form/form/FormList.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { fetchAllForms } from "@/../lib/actions/actions";
+import FormCard from "./FormCard";
+
+export default async function FormList() {
+  const forms = (await fetchAllForms()) as Form[];
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pt-6">
+      {forms.map((form) => (
+        <FormCard key={form.id} form={form} />
+      ))}
+    </div>
+  );
+}
