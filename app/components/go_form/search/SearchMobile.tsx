99998ed2293f67677f9e873555ae514670008e30
dvegaa20 <diego.vega.camacho@gmail.com>
2025-02-05 10:28:18 -0600

    feat(search): add SearchMobile component for mobile search functionality

---
+++ app/components/go_form/search/SearchMobile.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import { SearchIcon } from "lucide-react";
+import { useDialogStore } from "@/../store/store";
+
+export default function SearchMobile() {
+  const { open } = useDialogStore();
+  return (
+    <button onClick={open}>
+      <SearchIcon size={22} className="md:hidden text-neutral-500" />
+    </button>
+  );
+}
