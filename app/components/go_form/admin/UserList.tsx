1c3a12808152e8c8ba52fee6c9b32019428c93cc
dvegaa20 <diego.vega.camacho@gmail.com>
2025-02-05 10:24:15 -0600

    feat(admin): add UserList component for displaying a list of users

---
+++ app/components/go_form/admin/UserList.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import UserItem from "./UserItem";
+
+export default function UserList({
+  users,
+  onToggleBlock,
+  onToggleAdmin,
+  onDelete,
+}) {
+  return (
+    <div className="space-y-4">
+      {users.length === 0 ? (
+        <p className="text-center text-gray-500">No users found.</p>
+      ) : (
+        users.map((user) => (
+          <UserItem
+            key={user.id}
+            user={user}
+            onToggleBlock={onToggleBlock}
+            onToggleAdmin={onToggleAdmin}
+            onDelete={onDelete}
+          />
+        ))
+      )}
+    </div>
+  );
+}
