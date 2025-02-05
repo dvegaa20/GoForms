3a1d91873e18446999a4c474dbfebce572e7efb1
dvegaa20 <diego.vega.camacho@gmail.com>
2025-02-05 10:19:38 -0600

    feat(recent-forms): enhance RecentForms component with form selection and display

--- app/components/go_form/RecentForms.tsx
+++ app/components/go_form/RecentForms.tsx
@@ -1,5 +1,37 @@
 import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import FormList from "@/components/go_form/form/FormList";
 
 export default function RecentForms() {
-  return <div>RecentForms</div>;
+  return (
+    <section className="p-4 space-y-4 pt-8">
+      <div className="flex justify-between items-center">
+        <div className="bg-primary rounded-md px-4">
+          <p className="text-xl text-white font-medium">Your Forms</p>
+        </div>
+        <div className="flex items-center gap-x-12">
+          <Select defaultValue="anyone">
+            <SelectTrigger className="w-[200px] bg-white border border-gray-200 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              <SelectValue placeholder="Owned by anyone" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="anyone" className="cursor-pointer">
+                Owned by anyone
+              </SelectItem>
+              <SelectItem value="me" className="cursor-pointer">
+                Owned by me
+              </SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+      <FormList />
+    </section>
+  );
 }
