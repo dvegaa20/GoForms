96c12563559feb554871777b9ce7369d6e9f998c
dvegaa20 <diego.vega.camacho@gmail.com>
2025-02-06 12:37:50 -0600

    feat(FormId): add FormIdLayout and PublicFormIdPage components for form display and data fetching

---
+++ app/forms/[id]/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function FormIdLayout({
+  children,
+  params: { id },
+}: {
+  children: React.ReactNode;
+  params: { id: string };
+}) {
+  return <div className="bg-violet-100 min-h-screen px-6">{children}</div>;
+}
