ab9d7163e68cc3751e4723320446421f83452cae
dvegaa20 <diego.vega.camacho@gmail.com>
2025-02-06 14:14:24 -0600

    feat(Dashboard): implement user status check and display blocked user dialog

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { currentUser } from "@clerk/nextjs/server";
+import { fetchUserStatus } from "../../lib/actions/actions";
+import BlockedUserDialog from "@/components/go_form/BlockedUser";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="bg-violet-100 min-h-screen">{children}</div>;
+  const { id } = await currentUser();
+
+  let isBlocked = false;
+
+  try {
+    const status = await fetchUserStatus({ id });
+
+    if (status[0].status === "blocked") {
+      isBlocked = true;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+  return (
+    <div className="bg-violet-100 min-h-screen">
+      {isBlocked ? <BlockedUserDialog isBlocked={isBlocked} /> : children}
+    </div>
+  );
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,41 +1,13 @@
 import Header from "@/components/go_form/Header";
 import RecentForms from "@/components/go_form/RecentForms";
-import { currentUser } from "@clerk/nextjs/server";
-import { fetchUserStatus, postUserData } from "../../lib/actions/actions";
-import BlockedUserDialog from "@/components/go_form/BlockedUser";
 import React from "react";
 
 export default async function DashboardPage() {
-  const { id, firstName, lastName, emailAddresses } = await currentUser();
-
-  let isBlocked = false;
-
-  try {
-    const status = await fetchUserStatus({ id });
-
-    if (status[0].status === "blocked") {
-      isBlocked = true;
-    } else {
-      await postUserData({
-        id,
-        firstName,
-        lastName,
-        email: emailAddresses[0].emailAddress,
-      });
-    }
-  } catch (error) {
-    console.error(error);
-  }
-
   return (
     <div>
       <Header />
       <main className="max-w-7xl mx-auto ">
-        {isBlocked ? (
-          <BlockedUserDialog isBlocked={isBlocked} />
-        ) : (
-          <RecentForms />
-        )}
+        <RecentForms />
       </main>
     </div>
   );
