760c4c64f6fe4e09321fa0f7cd529083fc28deaa
dvegaa20 <diego.vega.camacho@gmail.com>
2025-02-06 12:37:59 -0600

    feat(FormSuccessPage): add success page component for form submission confirmation

---
+++ app/forms/[id]/success/page.tsx
@@ -0,0 +1,44 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { fetchFormById } from "@/../lib/actions/actions";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+export default async function FormSuccessPage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const form = (await fetchFormById({ id })) as Form[];
+
+  if (!form) {
+    notFound();
+  }
+
+  return (
+    <main className="max-w-3xl mx-auto pb-16 pt-8 space-y-3.5">
+      <Card>
+        <hr className="w-full border-t-8 rounded-t-xl border-violet-800" />
+        <CardHeader className="space-y-4">
+          <CardTitle className="text-3xl font-medium">
+            {form[0].title}
+          </CardTitle>
+
+          <CardDescription>Your response has been recorded.</CardDescription>
+
+          <Link
+            className="text-blue-600 underline text-sm"
+            replace
+            href={`/forms/${form[0].id}`}
+          >
+            Submit another response
+          </Link>
+        </CardHeader>
+      </Card>
+    </main>
+  );
+}
