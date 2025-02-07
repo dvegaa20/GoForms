a1a65bba58fb35bbf66d2e4b30cf756eb35ab6cb
dvegaa20 <diego.vega.camacho@gmail.com>
2025-02-07 13:04:59 -0600

    feat(BarChart): add BarChartComponent for visualizing response data

---
+++ app/components/go_form/form/responses/BarChart.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { BarChart, Card } from "@tremor/react";
+
+function BarChartComponent({ responses }: { responses: any }) {
+  return (
+    <Card>
+      <BarChart
+        className="mt-6"
+        data={responses}
+        index="value"
+        showLegend={false}
+        categories={["count"]}
+        colors={["blue-800"]}
+        yAxisWidth={48}
+      />
+    </Card>
+  );
+}
+
+export default BarChartComponent;
