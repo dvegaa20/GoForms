3491fc2e9ffe6b2bc2ac68cf317d661304f93ec2
dvegaa20 <diego.vega.camacho@gmail.com>
2025-02-10 09:29:31 -0600

    refactor(ResponsesTab): simplify tab structure and improve active state handling

--- app/components/go_form/form/tabs/ResponsesTab.tsx
+++ app/components/go_form/form/tabs/ResponsesTab.tsx
@@ -5,65 +5,39 @@ import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { cn } from "@/../lib/utils";
 import Link from "next/link";
 
-const tabs = [
-  {
-    title: "Summary",
-    href: "",
-  },
-  {
-    title: "Question",
-    href: "question",
-  },
-  {
-    title: "Individual",
-    href: "individual",
-  },
-] as const;
-
-function ResponsesTabs() {
+export default function ResponsesTabs() {
   const pathname = usePathname();
   const params = useParams();
+  const responsesPage = `/dashboard/forms/${params.id}/responses`;
+  const isActive = pathname === responsesPage;
 
   return (
     <Tabs defaultValue="questions" className="w-full">
       <TabsList className="w-full bg-transparent gap-x-4 !p-0 h-full justify-evenly">
-        {tabs.map((tab) => {
-          const responsesPage = `/dashboard/forms/${params.id}/responses`;
-          const isActive =
-            tab.href === ""
-              ? pathname === responsesPage
-              : pathname === `${responsesPage}/${tab.href}`;
-
-          return (
-            <TabsTrigger
-              key={tab.href}
-              value={tab.href}
-              className={cn(
-                "flex-col !p-0",
-                isActive ? "!text-blue-900" : "text-neutral-950"
-              )}
-              asChild
+        <TabsTrigger
+          value=""
+          className={cn(
+            "flex-col !p-0",
+            isActive ? "!text-blue-900" : "text-neutral-950"
+          )}
+          asChild
+        >
+          <div className="space-y-1.5 w-fit">
+            <Link
+              className="font-medium text-sm flex items-center px-2"
+              href={responsesPage}
             >
-              <div className="space-y-1.5 w-fit">
-                <Link
-                  className="font-medium text-sm flex items-center px-2"
-                  href={`${responsesPage}/${tab.href}`}
-                >
-                  {tab.title}
-                </Link>
-                <hr
-                  className={cn(
-                    "w-full border-b-2 rounded-t-xl",
-                    isActive ? "border-blue-900" : "border-transparent"
-                  )}
-                />
-              </div>
-            </TabsTrigger>
-          );
-        })}
+              Summary
+            </Link>
+            <hr
+              className={cn(
+                "w-full border-b-2 rounded-t-xl",
+                isActive ? "border-blue-900" : "border-transparent"
+              )}
+            />
+          </div>
+        </TabsTrigger>
       </TabsList>
     </Tabs>
   );
 }
-
-export default ResponsesTabs;
