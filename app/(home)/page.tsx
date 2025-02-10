a006933333a2ced5424b1d0d36e944d2ff4aa795
dvegaa20 <diego.vega.camacho@gmail.com>
2025-02-10 09:30:36 -0600

    refactor(LandingPage): remove AccessDenied component and simplify landing page structure

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,32 +4,19 @@ import Cta from "@/components/landing/Cta";
 import Footer from "@/components/landing/Footer";
 import Features from "@/components/landing/Features";
 import { AuroraBackground } from "@/components/ui/aurora-background";
-import AccessDenied from "@/components/AccessDenied";
 
-export default async function LandingPage({
-  searchParams,
-}: {
-  searchParams: Promise<{ error: string }>;
-}) {
-  const { error } = await searchParams;
-
+export default async function LandingPage() {
   return (
     <div className="flex flex-col">
-      {error ? (
-        <AccessDenied />
-      ) : (
-        <>
-          <Header />
-          <main className="flex-1 flex flex-col items-center">
-            <AuroraBackground>
-              <Hero />
-              <Features />
-              <Cta />
-            </AuroraBackground>
-          </main>
-          <Footer />
-        </>
-      )}
+      <Header />
+      <main className="flex-1 flex flex-col items-center">
+        <AuroraBackground>
+          <Hero />
+          <Features />
+          <Cta />
+        </AuroraBackground>
+      </main>
+      <Footer />
     </div>
   );
 }
