dbb19a0ccbabe27b16176febbb6909119b805eb2
dvegaa20 <diego.vega.camacho@gmail.com>
2025-02-10 09:29:02 -0600

    feat(Loading): add LoadingPage component and integrate it across various loading states

--- app/dashboard/new/layout.tsx
+++ app/dashboard/new/layout.tsx
@@ -1,5 +1,40 @@
 import React from "react";
+import { fetchUserStatus } from "@/../lib/actions/actions";
+import { currentUser } from "@clerk/nextjs/server";
+import BlockedUserDialog from "@/components/go_form/BlockedUser";
+import CreateNewFormHeader from "@/components/go_form/form/new/CreateNewFormHeader";
 
-export default function layout() {
-  return <div>layout</div>;
+export default async function NewFormLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { emailAddresses } = await currentUser();
+
+  let isBlocked = false;
+
+  try {
+    const status = await fetchUserStatus({
+      email: emailAddresses[0].emailAddress,
+    });
+
+    if (status[0].status === "blocked") {
+      isBlocked = true;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+  return (
+    <>
+      {isBlocked ? (
+        <BlockedUserDialog isBlocked={isBlocked} />
+      ) : (
+        <>
+          <CreateNewFormHeader />
+          <div className="bg-blue-100 min-h-screen">{children}</div>
+        </>
+      )}
+    </>
+  );
 }
