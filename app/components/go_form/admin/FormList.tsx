7df84307987bff95ac2b25f790862be059842dd3
dvegaa20 <diego.vega.camacho@gmail.com>
2025-02-13 13:07:11 -0600

    feat(NoForms): integrate NoForms component to display message when no forms are available in FormList

--- app/components/go_form/admin/FormList.tsx
+++ app/components/go_form/admin/FormList.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import FormItem from "./FormItem";
+import NoForms from "../NoForms";
 
 export default function FormList({ forms, onDelete }) {
   return (
     <div className="space-y-4">
       {forms.length === 0 ? (
-        <p className="text-center text-gray-500">No forms found.</p>
+        <NoForms />
       ) : (
         forms.map((form) => (
           <FormItem key={form.id} form={form} onDelete={onDelete} />
--- app/components/go_form/form/FormList.tsx
+++ app/components/go_form/form/FormList.tsx
@@ -1,14 +1,17 @@
 import FormCard from "./FormCard";
 import { useForms } from "@/../hooks/useForm";
-
+import NoForms from "../NoForms";
 export default function FormList() {
-  const forms = useForms();
+  const { forms } = useForms();
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pt-6">
-      {forms.map((form) => (
-        <FormCard key={form.id} form={form} />
-      ))}
-    </div>
+    <>
+      {forms.length === 0 && <NoForms />}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pt-6">
+        {forms.map((form) => (
+          <FormCard key={form.id} form={form} />
+        ))}
+      </div>
+    </>
   );
 }
