ef7568b99b302b30ad17ec795d10d99ef04bf220
dvegaa20 <diego.vega.camacho@gmail.com>
2025-02-13 13:07:28 -0600

    feat(LinkTabContent): add clipboard functionality to copy form link with success notification

--- app/components/go_form/form/tabs/LinkTabContent.tsx
+++ app/components/go_form/form/tabs/LinkTabContent.tsx
@@ -7,11 +7,13 @@ import { Input } from "@/components/ui/input";
 import { DialogClose } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
+import { Clipboard } from "lucide-react";
 
 export default function LinkTabContent() {
   const params = useParams();
   const id = params.id;
   const url = `${window.location.origin}/forms/${id}`;
+
   return (
     <TabsContent value="link" className="px-4 py-4 space-y-3">
       <div className="grid w-full items-center gap-3">
@@ -21,18 +23,20 @@ export default function LinkTabContent() {
         <Input type="url" id="link" value={url} readOnly />
       </div>
       <div className="flex items-end justify-end space-x-2">
-        <DialogClose asChild>
-          <Button size="sm">Cancel</Button>
-        </DialogClose>
         <Button
           size="sm"
+          variant="outline"
           onClick={() => {
             navigator.clipboard.writeText(url);
             toast.success("Link copied to clipboard");
           }}
         >
+          <Clipboard className="w-4 h-4" />
           Copy link
         </Button>
+        <DialogClose asChild>
+          <Button size="sm">Cancel</Button>
+        </DialogClose>
       </div>
     </TabsContent>
   );
