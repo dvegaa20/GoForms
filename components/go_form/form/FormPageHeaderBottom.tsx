81f88d751d73350cda74065490f02bc1d6c85474
dvegaa20 <diego.vega.camacho@gmail.com>
2025-02-19 22:12:53 -0600

    fix(responses): handle potential undefined responses length

--- components/go_form/form/FormPageHeaderBottom.tsx
+++ components/go_form/form/FormPageHeaderBottom.tsx
@@ -4,5 +4,5 @@ import FormTabs from "./FormTabs";
 export default async function FormPageHeaderBottom({ form }: { form: Form[] }) {
   const responses = await getResponses({ id: form[0].id });
 
-  return <FormTabs form={form} responses={responses.length} />;
+  return <FormTabs form={form} responses={responses?.length || 0} />;
 }
