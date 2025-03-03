8437d5d9bad1c871201490e279a07654bc15c5bd
dvegaa20 <diego.vega.camacho@gmail.com>
2025-03-03 11:54:36 -0600

    style(Header): adjust margin for header elements

--- components/go_form/Header.tsx
+++ components/go_form/Header.tsx
@@ -14,13 +14,13 @@ export default async function Header() {
 
   return (
     <header className="flex justify-between items-center sticky top-0 gap-x-4 py-2 bg-white z-50 w-full mx-auto shadow dark:bg-black">
-      <div className="flex items-center space-x-2">
+      <div className="flex items-center space-x-2 mx-3">
         <Link href={"/"} className="flex items-center space-x-2">
           <Layers className="h-6 w-6 text-primary" />
           <span className="ml-2 text-xl font-bold">GoForms</span>
         </Link>
       </div>
-      <div className="flex items-center space-x-4">
+      <div className="flex items-center space-x-4 mx-3">
         <Search />
         <div className="flex justify-end space-x-2">
           <LanguageSwitcher />
